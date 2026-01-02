be3362c33d1e52252b8f73270f996aafa990842b
Entlst <1515240396@qq.com>
2026-01-02 13:20:36 +0800

    initial

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    host: '0.0.0.0', // 监听所有地址
+    port: 5173,      // 端口号（默认是5173，可修改）
+    strictPort: true, // 如果端口被占用，直接退出
+  }
+})
